699bb5eefb18e4ab54f239bbfe02535ac539f332
= <=>
2024-04-08 15:57:06 -0600

    add segmented control to switch betweens cats and dogs

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { FlatList, SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import { useState } from 'react';
+import SegmentedControl from '@react-native-segmented-control/segmented-control';
 
 import Item from './Item';
 
-import { cats } from './breeds';
+import { cats, dogs } from './breeds';
 
 export default function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
   return (
     <SafeAreaView style={styles.container}>
+      <SegmentedControl
+        values={['Cats', 'Dogs']}
+        selectedIndex={selectedIndex}
+        onChange={(event) => {
+          setSelectedIndex(event.nativeEvent.selectedSegmentIndex);
+        }}
+      ></SegmentedControl>
       <FlatList
-        data={cats}
+        data={selectedIndex === 0 ? cats : dogs}
         renderItem={({ item, index }) => {
           return <Item item={item} index={index} />
         }}
